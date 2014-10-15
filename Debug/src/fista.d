1ef6a7fd74c770f45eb3bcb153631d45e76d03ed
soheluoa <sohelmsc@gmail.com>
2014-10-15 15:33:01 -0600

    added some untracked files

---
+++ Debug/src/fista.d
@@ -0,0 +1,28 @@
+src/fista.d: ../src/fista.cpp ../src/fistaParams.hpp ../src/fistaCore.hpp \
+ ../src/fdct3d.hpp ../src/commoninc.hpp ../src/numvec.hpp \
+ ../src/nummat.hpp ../src/numtns.hpp ../src/offvec.hpp ../src/offmat.hpp \
+ ../src/offtns.hpp ../src/fdct3dinline.hpp ../src/PARAMS.hpp
+
+../src/fistaParams.hpp:
+
+../src/fistaCore.hpp:
+
+../src/fdct3d.hpp:
+
+../src/commoninc.hpp:
+
+../src/numvec.hpp:
+
+../src/nummat.hpp:
+
+../src/numtns.hpp:
+
+../src/offvec.hpp:
+
+../src/offmat.hpp:
+
+../src/offtns.hpp:
+
+../src/fdct3dinline.hpp:
+
+../src/PARAMS.hpp:
---
+++ Debug/src/fistaCore.d
@@ -0,0 +1,29 @@
+src/fistaCore.d: ../src/fistaCore.cpp ../src/fistaParams.hpp \
+ ../src/fistaCore.hpp ../src/fdct3d.hpp ../src/commoninc.hpp \
+ ../src/numvec.hpp ../src/nummat.hpp ../src/numtns.hpp ../src/offvec.hpp \
+ ../src/offmat.hpp ../src/offtns.hpp ../src/fdct3dinline.hpp \
+ ../src/PARAMS.hpp
+
+../src/fistaParams.hpp:
+
+../src/fistaCore.hpp:
+
+../src/fdct3d.hpp:
+
+../src/commoninc.hpp:
+
+../src/numvec.hpp:
+
+../src/nummat.hpp:
+
+../src/numtns.hpp:
+
+../src/offvec.hpp:
+
+../src/offmat.hpp:
+
+../src/offtns.hpp:
+
+../src/fdct3dinline.hpp:
+
+../src/PARAMS.hpp:
