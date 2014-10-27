f67a0a110f1b2e60880eadbfd6694f9e0fec06aa
soheluoa <sohelmsc@gmail.com>
2014-10-27 14:33:27 -0600

    Added modified file

--- Debug/src/fdct3d_forward.d
+++ Debug/src/fdct3d_forward.d
@@ -1,11 +1,14 @@
-src/fdct3d_forward.d: ../src/fdct3d_forward.cpp ../src/fdct3d.hpp \
- ../src/commoninc.hpp ../src/numvec.hpp ../src/nummat.hpp \
- ../src/numtns.hpp ../src/offvec.hpp ../src/offmat.hpp ../src/offtns.hpp \
- ../src/fdct3dinline.hpp
+src/fdct3d_forward.d src/fdct3d_forward.o: ../src/fdct3d_forward.cpp \
+ ../src/fdct3d.hpp ../src/commoninc.hpp \
+ /global/software/fftw/fftw322/intel1214_shared_single/include/fftw3.h \
+ ../src/numvec.hpp ../src/nummat.hpp ../src/numtns.hpp ../src/offvec.hpp \
+ ../src/offmat.hpp ../src/offtns.hpp ../src/fdct3dinline.hpp
 
 ../src/fdct3d.hpp:
 
 ../src/commoninc.hpp:
+
+/global/software/fftw/fftw322/intel1214_shared_single/include/fftw3.h:
 
 ../src/numvec.hpp:
 
--- Debug/src/fdct3d_inverse.d
+++ Debug/src/fdct3d_inverse.d
@@ -1,11 +1,14 @@
-src/fdct3d_inverse.d: ../src/fdct3d_inverse.cpp ../src/fdct3d.hpp \
- ../src/commoninc.hpp ../src/numvec.hpp ../src/nummat.hpp \
- ../src/numtns.hpp ../src/offvec.hpp ../src/offmat.hpp ../src/offtns.hpp \
- ../src/fdct3dinline.hpp
+src/fdct3d_inverse.d src/fdct3d_inverse.o: ../src/fdct3d_inverse.cpp \
+ ../src/fdct3d.hpp ../src/commoninc.hpp \
+ /global/software/fftw/fftw322/intel1214_shared_single/include/fftw3.h \
+ ../src/numvec.hpp ../src/nummat.hpp ../src/numtns.hpp ../src/offvec.hpp \
+ ../src/offmat.hpp ../src/offtns.hpp ../src/fdct3dinline.hpp
 
 ../src/fdct3d.hpp:
 
 ../src/commoninc.hpp:
+
+/global/software/fftw/fftw322/intel1214_shared_single/include/fftw3.h:
 
 ../src/numvec.hpp:
 
